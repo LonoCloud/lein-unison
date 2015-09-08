519eca6e72456938cd8dadd175e72ab8e7fa1e95
Michael Drogalis <mjd3089@rit.edu>
2015-09-08 12:12:28 -0700

    Fix newlines in generated project file.

--- test/leiningen/unison_test.clj
+++ test/leiningen/unison_test.clj
@@ -34,14 +34,28 @@
         deps (mapv (fn [d] {:git (format "target/%s.git" d)}) followers)]
     (some-> (z/of-file f-name)
             (z/find-value z/next 'defproject)
+
+            z/rightmost
+            (z/insert-right :plugins)
+            z/rightmost
+            (z/insert-right [['lonocloud/lein-unison "0.1.11-SNAPSHOT"]])
+
+            (z/find-value z/next :plugins)
+            (z/prepend-newline)
+
             z/rightmost
             (z/insert-right :unison)
             z/rightmost
             (z/insert-right {:repos deps})
+
+            (z/find-value z/next :unison)
+            (z/prepend-newline)
+
             z/root
             ((fn [x] (spit f-name x))))))
 
 (defn initialize-repos [leader followers]
+  (println "Building new repository set...")
   (initialize-repo leader)
   (add-unison-to-project leader followers)
   (doseq [f followers]
