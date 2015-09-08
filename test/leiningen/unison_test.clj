dc835c41738db4e8542d2248e266ccd257a062d2
Michael Drogalis <mjd3089@rit.edu>
2015-09-08 08:34:25 -0700

    Modify project files to add dependencies.

--- test/leiningen/unison_test.clj
+++ test/leiningen/unison_test.clj
@@ -1,5 +1,6 @@
 (ns leiningen.unison-test
-  (:require [clojure.java.shell :refer [sh]]))
+  (:require [clojure.java.shell :refer [sh]]
+            [rewrite-clj.zip :as z]))
 
 (defn initialize-repo [repo-name]
   (sh "rm" "-rf" (str "target/" repo-name))
@@ -12,6 +13,19 @@
 
 (defn initialize-repos [leader followers]
   (initialize-repo leader)
-  (doseq [f followers] (initialize-repo f)))
+  (doseq [f followers]
+    (initialize-repo f)))
 
-(initialize-repos "a" [])
+#_(initialize-repos "a" ["b" "c" "d"])
+
+(defn depend-on-project [leader follower]
+  (let [f-name (str "target/" follower "/project.clj")]
+    (some-> (z/of-file f-name)
+            (z/find-value z/next 'defproject)
+            (z/find-value :dependencies)
+            z/right
+            (z/append-child [(symbol leader) "0.1.0-SNAPSHOT"])
+            z/root
+            ((fn [x] (spit f-name x))))))
+
+(depend-on-project "a" "b")
