644168eddb49eac9bf103219503b39e72d6935ee
Dave Zaharee <david.zaharee@viasat.com>
2018-06-08 14:04:11 +0000

    bump lein-voom version
    Old versions of lein-voom don't work with lein > 2.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[lein-voom "0.1.0-20150822_000839-g763d315"]
+  :dependencies [[lein-voom "0.1.0-20180511_185307-g3823723"]
                  [lein-update-dependency "0.1.2"]
                  [lein-pprint "1.1.1"]
                  [rewrite-clj "0.4.12"]]
